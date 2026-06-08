e64ed33df767afaee74cccd631c7d8ddac22fe02
Nassim El Ghzaoui <nas20001190@gmail.com>
2026-06-08 12:31:30 +0200

    next.js setup

---
+++ Code/login/Login.js
@@ -0,0 +1,16 @@
+export default function Login() {
+  
+  
+  
+  
+    return (
+    <div>
+      <h1>Inloggen</h1>
+      <form>
+        <input type="email" placeholder="E-mail" />
+        <input type="password" placeholder="Wachtwoord" />
+        <button type="submit">Inloggen</button>
+      </form>
+    </div>
+  )
+}
