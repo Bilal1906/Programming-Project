f2700fae66573109dc3f0019abd02c734c9f7564
Pasteque95 <yagiz.bicer@student.ehb.be>
2026-06-09 09:50:14 +0200

    feat: Basic layout van de dashboard

--- programming-project/app/Dashboard test/page.js
+++ programming-project/app/Dashboard test/page.js
@@ -1,1 +1,38 @@
-console.log("Pagina geladen");
+export default function Home() {
+  return (
+    <div>
+      {/* Topbar */}
+      <div
+        style={{
+          height: "60px",
+          backgroundColor: "#808080",
+          width: "100%",
+        }}
+      />
+
+      {/* Sidebar */}
+      <div
+        style={{
+          width: "200px",
+          height: "calc(100vh - 60px)",
+          backgroundColor: "#808080",
+          position: "fixed",
+          left: 0,
+          top: "60px",
+        }}
+      />
+
+      {/* Content */}
+      <div
+        style={{
+          marginLeft: "200px",
+          marginTop: "60px",
+          padding: "20px",
+          backgroundColor: "white",
+          minHeight: "calc(100vh - 60px)",
+        }}
+      >
+      </div>
+    </div>
+  );
+}
