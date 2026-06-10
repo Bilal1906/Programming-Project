7576648bd76ac679a5b4911b707d84ef57145ae5
BilalJaaboubEhB <bilal.jaaboub@student.ehb.be>
2026-06-10 20:42:26 +0200

    feat: student dashboard-first pagina aangemaakt

--- programming-project/app/student/dashboard/page.js
+++
@@ -1,8 +0,0 @@
-// app/student/page.js
-export default function StudentDashboard() {
-  return (
-    <div className="p-6">
-      <h1 className="text-xl font-bold text-gray-900">Dashboard</h1>
-    </div>
-  );
-}
