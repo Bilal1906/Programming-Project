06265ca823b12d4bf508519950599a65a077aa78
Nassim El Ghzaoui <nas20001190@gmail.com>
2026-06-10 18:55:58 +0200

    feat : sidebar & topbar in Profiel

---
+++ programming-project/app/stagementor/profiel/page.js
@@ -0,0 +1,13 @@
+import Topbar from "../component/topbar";
+
+export default function StagemenotDahsboard() {
+  return (
+    <div>
+      <Topbar 
+      title="Profiel" 
+      subtitle="2025 - 2026"
+      />
+      <h1>Profiel</h1>
+    </div>
+  );
+}
