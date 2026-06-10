5e30495a89f25086f153a730abc0cca8184ef1a0
Nassim El Ghzaoui <nas20001190@gmail.com>
2026-06-10 18:51:59 +0200

    feat : sidebar & topbar in Logboeken

---
+++ programming-project/app/stagementor/logboeken/page.js
@@ -0,0 +1,13 @@
+import Topbar from "../component/topbar";
+
+export default function StagemenotDahsboard() {
+  return (
+    <div>
+      <Topbar 
+      title="Logboeken" 
+      subtitle="2025 - 2026"
+      />
+      <h1>Logboeken</h1>
+    </div>
+  );
+}
