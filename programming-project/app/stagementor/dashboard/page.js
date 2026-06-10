db7bf2868a1c4fb51d6a7619018ad7174d56eab6
Nassim El Ghzaoui <nas20001190@gmail.com>
2026-06-10 18:21:55 +0200

    feat : topbar Stagementor

--- programming-project/app/stagementor/dashboard/page.js
+++ programming-project/app/stagementor/dashboard/page.js
@@ -1,7 +1,14 @@
+import Sidebar from "../component/sidebar";
+import Topbar from "../component/topbar";
+
 export default function StagemenotDahsboard() {
   return (
     <div>
-      <h1>Dahsboard</h1>
+      <Topbar 
+      title="Dashboard" 
+      subtitle="2025 - 2026"
+      />
+      <h1>Dashboard</h1>
     </div>
   );
 }
