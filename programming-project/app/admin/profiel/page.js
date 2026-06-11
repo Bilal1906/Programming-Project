bb8a1fc370a087a6c92073ee4d9ee8ef7f260711
Pasteque95 <yagiz.bicer@student.ehb.be>
2026-06-11 21:30:37 +0200

    :feat: profiel aangemaakt

---
+++ programming-project/app/admin/profiel/page.js
@@ -0,0 +1,12 @@
+import Topbar from "../component/topbar";
+
+export default function ProfielPage() {
+  return (
+    <>
+      <Topbar title="Profiel" />
+
+      <div className="p-6">
+      </div>
+    </>
+  );
+}
