56254c81353323c67ce63154e226e4df00b5c1b8
Pasteque95 <yagiz.bicer@student.ehb.be>
2026-06-11 21:28:34 +0200

    :feat: overeenkomsten aangemaakt

---
+++ programming-project/app/admin/overeenkomsten/page.js
@@ -0,0 +1,12 @@
+import Topbar from "../component/topbar";
+
+export default function OvereenkomstenPage() {
+  return (
+    <>
+      <Topbar title="Overeenkomsten" />
+
+      <div className="flex-1 p-6">
+      </div>
+    </>
+  );
+}
