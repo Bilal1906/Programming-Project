d1fa0cd7042a97c3b3f5bb4acfc9563206fbbcf1
BilalJaaboubEhB <bilal.jaaboub@student.ehb.be>
2026-06-11 13:48:23 +0200

    feat: topbar toegevoegd aan mijn stage pagina

--- programming-project/app/student/mijn-stage/page.js
+++ programming-project/app/student/mijn-stage/page.js
@@ -1,7 +1,17 @@
+'use client'
+
+import Topbar from '../component/topbar'
+
 export default function MijnStage() {
   return (
     <div className="flex-1 flex flex-col">
-      <h1>Mijn Stage</h1>
+      <Topbar
+        titel="Mijn Stage"
+        subtitel="Proximus NV · Toegepaste Informatica 3 · 2025-2026"
+      />
+      <div className="flex-1 bg-gray-100 p-6">
+        <p className="text-gray-400 text-sm">Inhoud komt hier.</p>
+      </div>
     </div>
   )
 }
