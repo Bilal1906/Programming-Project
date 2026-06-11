126a1e65f930963605474b1ab44762068dd4cb0e
Pasteque95 <yagiz.bicer@student.ehb.be>
2026-06-11 21:27:33 +0200

    :feat: docenten aangemaakt

---
+++ programming-project/app/admin/docenten/page.js
@@ -0,0 +1,12 @@
+import Topbar from "../component/topbar";
+
+export default function DocentenPage() {
+  return (
+    <>
+      <Topbar title="Docenten" />
+
+      <div className="flex-1 p-6">
+      </div>
+    </>
+  );
+}
