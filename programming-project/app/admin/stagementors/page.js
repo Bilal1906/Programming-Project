9ccca0a8f8f82a934b2548a50922336edc5ae965
Pasteque95 <yagiz.bicer@student.ehb.be>
2026-06-11 21:28:08 +0200

    :feat: stagementors aangemaakt

---
+++ programming-project/app/admin/stagementors/page.js
@@ -0,0 +1,12 @@
+import Topbar from "../component/topbar";
+
+export default function StagementorsPage() {
+  return (
+    <>
+      <Topbar title="Stagementors" />
+
+      <div className="flex-1 p-6">
+      </div>
+    </>
+  );
+}
