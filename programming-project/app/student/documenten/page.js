80751b2c58dd3055a079144d7030c0f12d3f201a
BilalJaaboubEhB <bilal.jaaboub@student.ehb.be>
2026-06-12 10:19:55 +0200

    feat: topbar toegevoegd aan documenten pagina

--- programming-project/app/student/documenten/page.js
+++ programming-project/app/student/documenten/page.js
@@ -1,7 +1,17 @@
+'use client'
+
+import Topbar from '../component/topbar'
+
 export default function Documenten() {
   return (
     <div className="flex-1 flex flex-col">
-      <h1>Documenten</h1>
+      <Topbar
+        titel="Documenten"
+        subtitel="Stageovereenkomsten & bijlagen · Proximus NV"
+      />
+      <div className="flex-1 bg-gray-100 p-6">
+        <p className="text-gray-400 text-sm">Inhoud komt hier.</p>
+      </div>
     </div>
   )
 }
