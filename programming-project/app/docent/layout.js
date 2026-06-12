97db87461794f55cfa4dbd335563616cabab6b5a
BilalJaaboubEhB <bilal.jaaboub@student.ehb.be>
2026-06-12 12:14:11 +0200

    feat: docent layout met sidebar en sticky navigatie

--- programming-project/app/docent/layout.js
+++ programming-project/app/docent/layout.js
@@ -1,9 +1,14 @@
-import "./global.css";
+import DocentSidebar from './component/sidebar'
 
 export default function DocentLayout({ children }) {
   return (
-    <div>
-      {children}
+    <div className="flex h-screen bg-gray-100">
+      <div className="sticky top-0 h-screen">
+        <DocentSidebar />
+      </div>
+      <main className="flex-1 flex flex-col overflow-y-auto">
+        {children}
+      </main>
     </div>
-  );
+  )
 }
