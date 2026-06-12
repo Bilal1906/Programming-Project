65748decb7a4ee7554d50922fb3636ece223fb8d
Pasteque95 <yagiz.bicer@student.ehb.be>
2026-06-12 10:05:56 +0200

    :fix: stagementors en stage pagina gefixt

--- programming-project/app/admin/stage/page.js
+++ programming-project/app/admin/stage/page.js
@@ -1,19 +1,12 @@
 import Topbar from "../component/topbar";
 
 export default function StagePage() {
-  const user = {
-    name: "Admin Gebruiker",
-  };
-
   return (
-    <div className="flex min-h-screen bg-gray-50">
+    <>
+      <Topbar title="Stage" />
 
-      <main className="flex-1 flex flex-col">
-        <Topbar title="Stage" />
-
-        <div className="flex-1 p-6">
-        </div>
-      </main>
-    </div>
+      <div className="p-6">
+      </div>
+    </>
   );
 }
