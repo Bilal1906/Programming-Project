782f05b0cd4d613080270fb7e0e287f1bf10fd19
Nassim El Ghzaoui <nas20001190@gmail.com>
2026-06-14 21:20:02 +0200

    feat(admin) : pages created & tested

---
+++ programming-project/app/admin/docenten/docent-bewerken/page.js
@@ -0,0 +1,12 @@
+import Topbar from "../../component/topbar";
+
+export default function DocentBewerkenPage() {
+  return (
+    <>
+      <Topbar title="Docent bewerken" />
+
+      <div className="flex-1 p-6">
+      </div>
+    </>
+  );
+}
