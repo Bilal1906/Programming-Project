268009abc038ccd415024be817efe854f2bae2f1
Nassim El Ghzaoui <nas20001190@gmail.com>
2026-06-21 22:43:18 +0200

    fix: page root redirige vers login

--- programming-project/app/page.js
+++ programming-project/app/page.js
@@ -1,9 +1,5 @@
+import { redirect } from 'next/navigation'
+
 export default function Home() {
-  return (
-    <>
-      <h1>test (bleu)</h1>
-      <h2>test (rouge)</h2>
-      <p>test (vert)</p>
-    </>
-  );
+  redirect('/authentificator/login')
 }
